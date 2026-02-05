918dffb3fe81c0f786ca19927c8cebf9ab5b3237
AryanSarafDev <aryansarafdev@gmail.com>
2026-02-06 01:59:02 +0530

    service start

--- src/main/java/com/prod/willdo/dto/TaskDto.java
+++ src/main/java/com/prod/willdo/dto/TaskDto.java
@@ -1,7 +1,7 @@
 package com.prod.willdo.dto;
 
 import jakarta.validation.constraints.NotBlank;
-
+//deprecated
 public record TaskDto(Long id, @NotBlank(message = "Description is required") String description, boolean completed) {
 
 }
